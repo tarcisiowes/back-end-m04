4a6105fdb0d42d8af2dcd89eb57391533b4e8c58
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-07-01 20:34:47 -0300

    Add files via upload

--- a03-back-integral-envio-emails-master/classe/src/nodemailer.js
+++ a03-back-integral-envio-emails-master/classe/src/nodemailer.js
@@ -1,12 +1,11 @@
 const nodemailer = require('nodemailer')
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.example.com",
+  host: "smtp.mailgun.org",
   port: 587,
   secure: false, // upgrade later with STARTTLS
   auth: {
-    user: "username",
-    pass: "password"
+
   }
 })
 

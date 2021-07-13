981b81a71574b2750160ecbd1d2d0ae2eb32d18d
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-07-13 17:36:00 -0300

    yup-user

--- a06-back-integral-validacoes-boas-praticas/classe/validacoes/schemas.js
+++ a06-back-integral-validacoes-boas-praticas/classe/validacoes/schemas.js
@@ -1,5 +1,22 @@
 const yup = require('./configuracoes')
 
 const schemaCadastroUsuario = yup.object().shape({
-  
+
+  nome: yup.string().required(),
+  email: yup.string().email().required(),
+  senha: yup.string().min(8).required(),
+  nome_loja: yup.string().required()
 })
+
+const schemaAtualizarUsuario = yup.object().shape({
+
+  nome: yup.string(),
+  email: yup.string().email(),
+  senha: yup.string().min(8),
+  nome_loja: yup.string()
+})
+
+module.exports = {
+  schemaCadastroUsuario,
+  schemaAtualizarUsuario
+}

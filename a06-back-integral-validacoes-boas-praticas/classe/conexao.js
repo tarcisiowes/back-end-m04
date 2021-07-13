86cdb4fc482edda1774088298a88a9ad0d5d9d79
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-07-13 17:19:20 -0300

    Add files via upload

---
+++ a06-back-integral-validacoes-boas-praticas/classe/conexao.js
@@ -0,0 +1,15 @@
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host: 'ec2-52-45-179-101.compute-1.amazonaws.com',
+    user: 'wnabpdrpcyoklj',
+    password: '0aa9a16562d0e8ddc1d81edc6fe1c81fbe358c3ae7d139df29e10b38a47a8df8',          
+    database: 'd3s3mi5q8k71c4',
+    Port: 5432,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
+})
+
+module.exports = knex

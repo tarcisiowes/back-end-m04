96a60168ef179cae2e4d28c95fe2481e05849b02
Tarcisio Wesley <47664070+tarushii@users.noreply.github.com>
2021-07-15 16:45:54 -0300

    login

---
+++ a07-codigo-inicial-refatoracao/src/controladores/autenticacao.js
@@ -0,0 +1,39 @@
+const knex = require('../bancodedados/conexao')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+const login = async (req, res) => {
+  const { email, senha } = req.body
+  
+  //TODO - implementar validaçao com yup
+  if (!email || !senha) {
+    return res.status(404).json('É obrigatório email e senha');
+  }
+  
+  try {
+    const usuario = await knex('usuarios').where({ email }).first();
+    
+    if (!usuario) {
+      return res.status(404).json('O usuario não foi encontrado');
+    }
+    
+    const senhaConfere = await bcrypt.compare(senha, usuario.senha);
+    
+    if (!senhaConfere) {
+      return res.status(400).json("Email e senha não confere");
+    }
+    
+    const token = jwt.sign({ id: usuario.id }, process.env.SENHA_JWT, { expiresIn: '8h' });
+    
+    const { senha: _, ...dadosUsuario } = usuario;
+    
+    return res.status(200).json({
+      usuario: dadosUsuario,
+      token
+    })
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+}
+
+module.exports = { login }
--- a07-codigo-inicial-refatoracao/src/index.js
+++ a07-codigo-inicial-refatoracao/src/index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json())
 
 const filtroLogin = require('./filtros/filtrologin')
+const autenticacaoControlador = require('./controladores/autenticacao')
 
 const hash = 'uV9R6dJBs7erwsBw8giLZyexOjw';
 
@@ -73,42 +74,7 @@ app.post('/usuarios', async (req, res) => {
   }
 });
 
-// login
-app.post('/login', async (req, res) => {
-  const {
-    email,
-    senha
-  } = req.body;
-  
-  if (!email || !senha) {
-    return res.status(404).json('É obrigatório email e senha');
-  }
-  
-  try {
-    const usuario = await knex('usuarios').where({ email }).first();
-    
-    if (!usuario) {
-      return res.status(404).json('O usuario não foi encontrado');
-    }
-    
-    const senhaCorreta = await bcrypt.compare(senha, usuario.senha);
-    
-    if (!senhaCorreta) {
-      return res.status(400).json("Email e senha não confere");
-    }
-    
-    const token = jwt.sign({ id: usuario.id }, hash, { expiresIn: '8h' });
-    
-    const { senha: _, ...dadosUsuario } = usuario;
-    
-    return res.status(200).json({
-      usuario: dadosUsuario,
-      token
-    });
-  } catch (error) {
-    return res.status(400).json(error.message);
-  }
-});
+app.post('/login', autenticacaoControlador.login);
 
 app.use(filtroLogin)
 

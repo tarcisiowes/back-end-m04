9e2bb1124a48918c68179c6c7a18464a2b166351
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-07-18 20:04:33 -0300

    func send and delet image

--- a05-back-integral-upload-arquivos/classe/src/services/aws.js
+++ a05-back-integral-upload-arquivos/classe/src/services/aws.js
@@ -6,3 +6,21 @@ const s3 = new aws.s3({
   accessKeyId: process.env.SPACES_KEY,
   secretAccessKey: process.env.SPACES_SECRET
 })
+
+const sendImage = async (nome, imagem) => {
+  return await s3.putObject({
+    Bucket: process.env.SPACES_BUCKET,
+    key: nome,
+    body: imagem,
+    ACL: 'public-read'
+  }).promise()
+}
+
+const deletImage = async (nome) => {
+    return await s3.deletObject({
+    Bucket: process.env.SPACES_BUCKET,
+    key: nome
+  }).promise()
+}
+
+module.exports = { sendImage, deletImage }

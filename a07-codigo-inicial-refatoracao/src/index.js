eeb3dcca4351500939f4099926a484688be5e38a
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-07-22 10:14:04 -0300

    rotas atualizadas

--- a07-codigo-inicial-refatoracao/src/index.js
+++ a07-codigo-inicial-refatoracao/src/index.js
@@ -8,22 +8,23 @@ app.use(express.json())
 
 const filtroLogin = require('./filtros/filtrologin')
 const autenticacaoControlador = require('./controladores/autenticacao')
+const usuarioControlador = require('./controladores/usuarios')
 
 const hash = 'uV9R6dJBs7erwsBw8giLZyexOjw'
 
 
 
 // endpoint para cadastro de usuario
-app.post('/usuarios', );
-
+app.post('/usuarios', usuarioControlador.cadastrarUsuario);
+// login
 app.post('/login', autenticacaoControlador.login);
 
 app.use(filtroLogin)
 
 // obter perfil do usuario logado pelo token
-app.get('/perfil', );
+app.get('/perfil', usuarioControlador.obterPerfil);
 
 // atualizar perfil do usuario logado
-app.put('/perfil', );
+app.put('/perfil', usuarioControlador.atualizarPefil);
 
 app.listen(process.env.PORT);

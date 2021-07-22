de7c23fa05d905baaa3c3745244910ae52c226c5
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-07-22 10:38:15 -0300

    refatoração de rotas

--- a07-codigo-inicial-refatoracao/src/index.js
+++ a07-codigo-inicial-refatoracao/src/index.js
@@ -1,30 +1,10 @@
 require('dotenv').config()
 const express = require('express')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
 const app = express()
+const rotas = require('./rotas')
 
 app.use(express.json())
 
-const filtroLogin = require('./filtros/filtrologin')
-const autenticacaoControlador = require('./controladores/autenticacao')
-const usuarioControlador = require('./controladores/usuarios')
-
-const hash = 'uV9R6dJBs7erwsBw8giLZyexOjw'
-
-
-
-// endpoint para cadastro de usuario
-app.post('/usuarios', usuarioControlador.cadastrarUsuario);
-// login
-app.post('/login', autenticacaoControlador.login);
-
-app.use(filtroLogin)
-
-// obter perfil do usuario logado pelo token
-app.get('/perfil', usuarioControlador.obterPerfil);
-
-// atualizar perfil do usuario logado
-app.put('/perfil', usuarioControlador.atualizarPefil);
+app.use(rotas)
 
 app.listen(process.env.PORT);
--- a07-codigo-inicial-refatoracao/src/rotas.js
+++ a07-codigo-inicial-refatoracao/src/rotas.js
@@ -1,1 +1,15 @@
-.
+const express = require('express')
+const rotas = express()
+
+const filtroLogin = require('./filtros/filtrologin')
+const autenticacaoControlador = require('./controladores/autenticacao')
+const usuarioControlador = require('./controladores/usuarios')
+
+rotas.post('/usuarios', usuarioControlador.cadastrarUsuario);
+rotas.post('/login', autenticacaoControlador.login);
+
+rotas.use(filtroLogin)
+rotas.get('/perfil', usuarioControlador.obterPerfil);
+rotas.put('/perfil', usuarioControlador.atualizarPefil)
+
+module.exports = rotas
